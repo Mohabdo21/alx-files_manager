f119ddb12f8ca835f254fb21295bc9a6dfe1e47f
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-08-03 16:05:34 +0400

    handle a broader range of characters by 'utf-8' for auth decoding

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -19,7 +19,7 @@ class AuthController {
     if (!authHeader || !authHeader.startsWith('Basic ')) return res.status(401).json({ error: 'Unauthorized' });
 
     const [email, password] = Buffer.from(authHeader.split(' ')[1], 'base64')
-      .toString('ascii')
+      .toString('utf-8')
       .split(':');
 
     try {
