1504fd697220eb426699f6001b6a2908625683bd
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-08-06 12:29:38 +0400

    return file "getShow"

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -124,14 +124,7 @@ class FilesController {
 
       if (!file) return res.status(404).json({ error: 'Not found' });
 
-      return res.status(200).json({
-        id: file._id,
-        userId: file.userId,
-        name: file.name,
-        type: file.type,
-        isPublic: file.isPublic,
-        parentId: file.parentId,
-      });
+      return res.status(200).json(file);
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: 'Server error' });
